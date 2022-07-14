5ec8efc94b30884479be75c9ba6b0fd28ee27418
Oleksa <oleksa.antsupov@gmail.com>
2022-07-14 15:49:47 +0300

    add component Card

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Card from './components/card';
+import Pagination from './components/pagination';
+
+import './App.css';
+
+const product = {
+  "id": "76w0hz7015kkr9kjkav",
+  "images": [
+    "https://content2.rozetka.com.ua/goods/images/big_tile/163399632.jpg",
+    "https://content.rozetka.com.ua/goods/images/big_tile/163399633.jpg"
+  ],
+  "title": "Ноутбук Acer Aspire 3 A315-57G-336G (NX.HZREU.01S) Charcoal Black",
+  "rating": 2.89,
+  "price": 15999,
+  "category": "laptops",
+  "brand": "acer"
+};
+
+function App() {
+  const pageSize = 9;
+
+  return (
+    <div className="App">
+     <Card product={product} />
+     <Pagination />
+    </div>
+  );
+}
+
+export default App;
